3abc2d141ce2b054a3ad26bd79abe998c228e806
Daniel Moylan <demoylan@gmail.com>
2024-09-27 16:55:32 -0400

    init

---
+++ main.js
@@ -0,0 +1,44 @@
+
+const main = () => {
+    const buttons = document.querySelector( "nav" ).children;
+    const pages = document.querySelector( "main" ).children;
+
+    let last_btn = pages[ 0 ];
+    let last_page = buttons[ 0 ];
+
+    for ( let i = 0; i < buttons.length; i++ ) {
+        buttons[ i ].onclick = ( () => ( e ) => {
+            last_btn.classList.remove( "selected" );
+            last_page.classList.remove( "selected" );
+            buttons[ i ].classList.add( "selected" );
+            pages[ i ].classList.add( "selected" );
+
+            last_btn = buttons[ i ];
+            last_page = pages[ i ];
+        } )();
+    }
+
+    // page 2 form data tracker
+    ( () => {
+        let value_one = "Cheese";
+        let value_two = "Cheese";
+        const plugs = document.getElementsByClassName( "elm_to_insert" );
+
+        for ( const elm of pages[ 1 ].querySelector( "form" ).children ) {
+            elm.onchange = ( e ) => {
+                if ( e.target.name === "cheese" ) {
+                    value_one = e.target.value;
+                } else if ( e.target.name === "cheese_two" ) {
+                    value_two = e.target.value;
+                }
+
+                for ( let i = 0; i < plugs.length; i++ ) {
+                    plugs[ i ].innerHTML = `You picked ${value_one} ` + 
+                                           `and ${value_two}`;
+                }
+            };
+        }
+    } )();
+};
+
+window.onload = main;
