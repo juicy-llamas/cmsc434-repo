c253a2eb02f29a74b6ccd44e87fee119cf210836
Jay Patel <j012.patel@gmail.com>
2024-09-29 20:55:44 -0400

    TO-DO Part

--- main.js
+++ main.js
@@ -1,44 +1,93 @@
 
 const main = () => {
-    const buttons = document.querySelector( "nav" ).children;
-    const pages = document.querySelector( "main" ).children;
+    const buttons = document.querySelector("nav").children;
+    const pages = document.querySelector("main").children;
 
-    let last_btn = pages[ 0 ];
-    let last_page = buttons[ 0 ];
+    let last_btn = pages[0];
+    let last_page = buttons[0];
 
-    for ( let i = 0; i < buttons.length; i++ ) {
-        buttons[ i ].onclick = ( () => ( e ) => {
-            last_btn.classList.remove( "selected" );
-            last_page.classList.remove( "selected" );
-            buttons[ i ].classList.add( "selected" );
-            pages[ i ].classList.add( "selected" );
+    for (let i = 0; i < buttons.length; i++) {
+        buttons[i].onclick = (() => (e) => {
+            last_btn.classList.remove("selected");
+            last_page.classList.remove("selected");
+            buttons[i].classList.add("selected");
+            pages[i].classList.add("selected");
 
-            last_btn = buttons[ i ];
-            last_page = pages[ i ];
-        } )();
+            last_btn = buttons[i];
+            last_page = pages[i];
+        })();
+
     }
 
     // page 2 form data tracker
-    ( () => {
+    (() => {
         let value_one = "Cheese";
         let value_two = "Cheese";
-        const plugs = document.getElementsByClassName( "elm_to_insert" );
+        const plugs = document.getElementsByClassName("elm_to_insert");
 
-        for ( const elm of pages[ 1 ].querySelector( "form" ).children ) {
-            elm.onchange = ( e ) => {
-                if ( e.target.name === "cheese" ) {
+        for (const elm of pages[1].querySelector("form").children) {
+            elm.onchange = (e) => {
+                if (e.target.name === "cheese") {
                     value_one = e.target.value;
-                } else if ( e.target.name === "cheese_two" ) {
+                } else if (e.target.name === "cheese_two") {
                     value_two = e.target.value;
                 }
 
-                for ( let i = 0; i < plugs.length; i++ ) {
-                    plugs[ i ].innerHTML = `You picked ${value_one} ` + 
-                                           `and ${value_two}`;
+                for (let i = 0; i < plugs.length; i++) {
+                    plugs[i].innerHTML = `You picked ${value_one} ` +
+                        `and ${value_two}`;
                 }
             };
         }
-    } )();
+
+    })();
+    initializeToDo();
+};
+// TO-DO
+const initializeToDo = () => {
+    const todoList = document.querySelector('.todo-list');
+
+    const addCloseButtons = () => {
+        const items = document.querySelectorAll('.todo-item');
+        items.forEach(item => {
+            if (!item.querySelector('.close')) {
+                const span = document.createElement("SPAN");
+                const txt = document.createTextNode("\u00D7");
+                span.className = "close";
+                span.appendChild(txt);
+                item.appendChild(span);
+
+                span.onclick = function () {
+                    item.style.display = "none";
+                }
+            }
+        });
+    };
+
+    addCloseButtons();
+
+    todoList.addEventListener('click', function (ev) {
+        if (ev.target.tagName === 'LI') {
+            ev.target.classList.toggle('checked');
+        }
+    }, false);
+    window.newElement = function () {
+        const li = document.createElement("li");
+        li.className = 'todo-item';
+        const inputValue = document.getElementById("myInput").value;
+        const t = document.createTextNode(inputValue);
+
+        if (inputValue === '') {
+            alert("Please enter a task?");
+            return;
+        }
+
+        li.appendChild(t);
+        todoList.appendChild(li);
+        document.getElementById("myInput").value = "";
+
+        addCloseButtons();
+    };
 };
 
 window.onload = main;
